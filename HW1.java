92b5d33a73850dcb5a62380bc9d7bd23f82f1176
DimaD1989 <d_d1989sv@mail.ru>
2022-11-25 15:45:43 +0300

    task_1

---
+++ HW1.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Random;
+
+// 1. Выбросить случайное целое число в диапазоне от -1000 до 1000 и сохранить в i
+// 2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+// 3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+// 4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
+
+// Пункты реализовать в методе main
+// *Пункты реализовать в разных методах
+
+// int i = new Random().nextInt(k); //это кидалка случайных чисел!)
+// import java.util.Random;
+// import java.util.Arrays;
+
+public class HW1 {
+    public static void main(String[] args)
+    {
+        int[] array  = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rnd(-1000,1000);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    
+    public static int rnd(int min, int max)
+    {
+        Random random = new Random();
+        return random.nextInt(max - min +1) + min;  
+    }
+    
+}
