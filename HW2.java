fc02f4cd5ec7acea077a70fa5910417e655477d3
DimaD1989 <d_d1989sv@mail.ru>
2022-11-29 20:30:23 +0300

    endHW2

--- HW2.java
+++ HW2.java
@@ -1,17 +1,31 @@
+
 import java.util.Arrays;;
 public class HW2{
+    public static  String[] forStar = Calc(3, 56);
+    
     /**
      * @param args
      */
     public static void main(String[] args){
-        //task1();
-        // isRevers();
-        StringBuilder sb = new StringBuilder("Stroka");
+        //        5
+        System.out.println("Task5");
+        String str5 = "3 + 56 = 59";
+        changeEquals(str5);
+//        6
+        System.out.println("Task6");
+        String str6 = "3 + 56 = 59";
+        System.out.println(replaceEquals(str6));
+        task1();
+        isRevers();
+        StringBuilder sb = new StringBuilder("строка");
+        isReversRecurs(sb);
         System.out.println(isReversRecurs(sb));
+        Calc(3, 56);
+        System.out.println(Arrays.toString(Calc(3, 56)));
     }
     static void task1(){    
-        String stringFirst = "naiti naimenshee okno v stroke";
-        String stringSecond = "sodergashee vse simvols okno v drugoi stroke navernoe";
+        String stringFirst = "найти наименьшее окно в строке";
+        String stringSecond = "содержащей все символы окно  другой строке наверное";
         String[] arr1 = stringFirst.split(" ");
         String[] arr2 = stringSecond.split(" ");
         String[] arr3 = new String[arr1.length > arr2.length ? arr1.length : arr2.length];
@@ -38,8 +52,8 @@ public class HW2{
 
 static void isRevers (){
     // Напишите программу на Java, чтобы проверить, являются ли две данные строки вращением друг друга.
-    String stringFirst = "naiti naimenshee okno v stroke";
-    String stringSecond = "";
+    String stringFirst = "найти наименьшее окно в строке";
+    String stringSecond = "екортс в онко еешьнемиан итйан";
     StringBuilder sb = new StringBuilder(stringFirst);
     String stringTemp = sb.reverse().toString();
     System.out.println(stringSecond.compareTo(stringTemp) == 0);
@@ -53,5 +67,67 @@ static StringBuilder isReversRecurs (StringBuilder sb) {
         return sb;
     }
     return isReversRecurs(sb.deleteCharAt(sb.length()-1));
+    }
+    /**
+     * @param x
+     * @param y
+     * @return
+     */
+    static String[] Calc(int x, int y) {
+//    Дано два числа, например 3 и 56, необходимо составить следующие строки: 3 + 56 = 59 3 – 56 = -53 3 * 56 = 168 Используем метод StringBuilder.append().
+                String[] result = new String[3];
+        
+                StringBuilder sb = new StringBuilder();
+                sb.append(x);
+                sb.append(" + ");
+                sb.append(y);
+                sb.append(" = ");
+                sb.append(x+y);
+                StringBuilder alfa = new StringBuilder();
+                alfa.append(x);
+                alfa.append(" - ");
+                alfa.append(y);
+                alfa.append(" = ");
+                alfa.append(x-y);
+                StringBuilder mult = new StringBuilder();
+                mult.append(x);
+                mult.append(" * ");
+                mult.append(y);
+                mult.append(" = ");
+                mult.append(x*y);
+        
+                result[0] = sb.toString();
+                result[1] = alfa.toString();
+                result[2] = mult.toString();
+        
+                return result;
+                // System.out.println(result[0]);
+                // System.out.println(result[1]);
+                // System.out.println(result[2]);
+            }
+/**
+ * 
+ */
+static void changeFignyaFitchaStar(){
+    for (int i = 0; i < forStar.length; i++) {
+        StringBuilder sb = new StringBuilder(forStar[i]);
+        sb.replace(7, 8, "Равно");
+        forStar[i] = sb.toString();
+        }
+    }
+    //  5. Замените символ “=” на слово “равно”. Используйте методы StringBuilder.insert(),StringBuilder.deleteCharAt().
+    static void changeEquals(String x){
+        StringBuilder sb = new StringBuilder(x);
+        int eq = sb.indexOf("=");
+        sb.deleteCharAt(eq);
+        sb.insert(eq, "равно");
+        System.out.println(sb.toString());
+    }
+//  6. *Замените символ “=” на слово “равно”. Используйте методы StringBuilder.replace().
+    static String replaceEquals(String x){
+        StringBuilder sb = new StringBuilder(x);
+        int eq = sb.indexOf("=");
+        sb.replace(eq, eq+1,"равно" );
+        return sb.toString();
     }
 }
