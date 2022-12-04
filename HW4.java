8adb4e189043ce56a851ff4ae00a5e0b758bd773
DimaD1989 <d_d1989sv@mail.ru>
2022-12-04 12:09:29 +0300

    start

---
+++ HW4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+public class HW4 {
+    // Написать приложение для ввода ФИО, возраст и пол пользователей. Данные хранить в разных списках. 
+    // Сортировать пользователей по возрасту. Не вывести в порядке возрастания возрастов а именно сортировать!)
+
+    
+}
